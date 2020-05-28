1fbf9ef5afcdaca5252dc4964678b25ac80ab0b1
koco <koco@ciklum.com>
2020-05-28 12:39:41 +0300

    Move Dockerfile and docker-compose.yml to the root of the project. Add missing surefire maven plugin to run tests. Avoid System.exit in the main method in order to facilitate testing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM java:8-jdk-alpine
-COPY ../target/coding-0.0.1-SNAPSHOT-jar-with-dependencies.jar /usr/app/
+COPY target/coding-0.0.1-SNAPSHOT-jar-with-dependencies.jar /usr/app/
 WORKDIR /usr/app
 ENTRYPOINT ["java", "-jar", "coding-0.0.1-SNAPSHOT-jar-with-dependencies.jar"]
--- src/main/java/com/verygood/security/coding/CodingApplication.java
+++ src/main/java/com/verygood/security/coding/CodingApplication.java
@@ -5,23 +5,18 @@ import com.verygood.security.coding.api.IEncryptionKeyService;
 import com.verygood.security.coding.api.exception.WrongInputArgumentsException;
 import com.verygood.security.coding.service.EncryptionKeyServiceImpl;
 import com.verygood.security.coding.service.TripleDesEncryptionImpl;
+import org.apache.commons.lang3.StringUtils;
 
 public class CodingApplication {
 
     static final String ENCRYPTION_KEY_ENV_VARIABLE = "ENCRYPTION_KEY";
     static final String TEXT_TO_ENCRYPT_ENV_VARIABLE = "TEXT_TO_ENCRYPT";
+    private static String encryptionKey;
+    private static String textToEncrypt;
 
     public static void main(String[] args) {
-        if (args.length <= 1) {
-            readArgsFromEnvironmentVariables(args);
-            if (args.length <= 1) {
-                throw new WrongInputArgumentsException("Arguments are empty or half empty. Please input an encryption key and a text, "
-                        + "e.g. ./application.jar FHDGYR 'text to encrypt'");
-            }
-        }
+        setArguments(args);
 
-        String encryptionKey = args[0];
-        String textToEncrypt = args[1];
         IEncryptionKeyService encryptionKeyService = new EncryptionKeyServiceImpl(encryptionKey);
         Encryption encryptionService = new TripleDesEncryptionImpl(encryptionKeyService);
         String encryptedText = encryptionService.encrypt(textToEncrypt);
@@ -29,13 +24,26 @@ public class CodingApplication {
 
         System.out.println("Encrypted text: " + encryptedText);
         System.out.println("Decrypted text: " + decryptedText);
-        System.exit(0);
     }
 
-    private static void readArgsFromEnvironmentVariables(String[] args) {
-        String encryptionKey = System.getenv(ENCRYPTION_KEY_ENV_VARIABLE);
-        String textToEncrypt = System.getenv(TEXT_TO_ENCRYPT_ENV_VARIABLE);
-        args[0] = encryptionKey;
-        args[1] = textToEncrypt;
+    private static void setArguments(String[] args) {
+        if (args.length <= 1) {
+            readArgsFromEnvironmentVariables();
+            if (StringUtils.isAllBlank(encryptionKey, textToEncrypt)) {
+                throw new WrongInputArgumentsException("Arguments are empty or half empty. Please input an encryption key and a text, "
+                        + "e.g. ./application.jar FHDGYR 'text to encrypt'");
+            }
+        }
+        else {
+            encryptionKey = args[0];
+            textToEncrypt = args[1];
+        }
+    }
+
+    private static void readArgsFromEnvironmentVariables() {
+        String encryptionKeyFromEnv = System.getenv(ENCRYPTION_KEY_ENV_VARIABLE);
+        String textToEncryptFromEnv = System.getenv(TEXT_TO_ENCRYPT_ENV_VARIABLE);
+        encryptionKey = encryptionKeyFromEnv;
+        textToEncrypt = textToEncryptFromEnv;
     }
 }
